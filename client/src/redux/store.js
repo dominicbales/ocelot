487289552c47e3e63b15ce49cdd7db3fd1700861
SoteroX <soterox13@gmail.com>
2019-07-27 22:10:36 -0500

    disable redux tools in production

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -17,7 +17,9 @@ export const store = createStore(
   rootReducer,
   compose(
     applyMiddleware(...middlewares),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    process.env.NODE_ENV !== "production" &&
+      window.__REDUX_DEVTOOLS_EXTENSION__ &&
+      window.__REDUX_DEVTOOLS_EXTENSION__()
   )
 );
 

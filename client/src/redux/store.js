cc9f0cc16465bf6b613053164625c082399e815b
SoteroX <soterox13@gmail.com>
2019-07-27 22:19:22 -0500

    new redux tools

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,6 +1,7 @@
 import rootReducer from "./reducers/index";
 import { createStore, applyMiddleware, compose } from "redux";
 import thunk from "redux-thunk";
+// import {} from 'redux-devtools-extension/developmentOnly'
 import { persistStore, persistReducer } from "redux-persist";
 import storage from "redux-persist/lib/storage";
 
@@ -11,16 +12,16 @@ const persistConfig = {
 
 // const persistedReducer = persistReducer(persistConfig, rootReducer);
 const middlewares = [thunk];
-
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 export const store = createStore(
   // persistedReducer,
   rootReducer,
-  compose(
-    applyMiddleware(...middlewares),
-    process.env.NODE_ENV !== "production" &&
-      window.__REDUX_DEVTOOLS_EXTENSION__ &&
-      window.__REDUX_DEVTOOLS_EXTENSION__()
-  )
+  composeEnhancers(applyMiddleware(...middlewares))
+  // compose(
+  //   applyMiddleware(...middlewares),
+  //     window.__REDUX_DEVTOOLS_EXTENSION__ &&
+  //     window.__REDUX_DEVTOOLS_EXTENSION__()
+  // )
 );
 
 // export let persistor = persistStore(store);

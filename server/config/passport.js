aebc171f1d7152c8c369418ba93a4abd4a10758b
SoteroX <soterox13@gmail.com>
2019-07-31 22:57:59 -0500

    remove keys import from passport

--- server/config/passport.js
+++ server/config/passport.js
@@ -1,9 +1,8 @@
-const JwtStrategy = require('passport-jwt').Strategy;
-const ExtractJwt = require('passport-jwt').ExtractJwt;
-const mongoose = require('mongoose');
-const User = mongoose.model('User');
-const keys = require('./keys.js');
-
+const JwtStrategy = require("passport-jwt").Strategy;
+const ExtractJwt = require("passport-jwt").ExtractJwt;
+const mongoose = require("mongoose");
+const User = mongoose.model("User");
+// const keys = require('./keys.js');
 
 // var cookieExtractor = function(req) {
 //     var token = null;
@@ -13,20 +12,23 @@ const keys = require('./keys.js');
 
 const opts = {};
 // check token in cookie
-// opts.jwtFromRequest = cookieExtractor; 
+// opts.jwtFromRequest = cookieExtractor;
 opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
-opts.secretOrKey = keys.SECRET_KEY;
+opts.secretOrKey = process.env.SECRET_KEY;
+// opts.secretOrKey = keys.SECRET_KEY;
 
 module.exports = passport => {
-    passport.use(new JwtStrategy(opts, (jwt_payload, done) => {
-        User.findById(jwt_payload._id)
-            .then(user => {
-                if(user){
-                    return done(null, user);
-                }
-                console.log('wrong')
-                return done(null, false);
-            })
-            .catch(err => console.log('Error: ', err));
-    }));
-}
+  passport.use(
+    new JwtStrategy(opts, (jwt_payload, done) => {
+      User.findById(jwt_payload._id)
+        .then(user => {
+          if (user) {
+            return done(null, user);
+          }
+          console.log("wrong");
+          return done(null, false);
+        })
+        .catch(err => console.log("Error: ", err));
+    })
+  );
+};

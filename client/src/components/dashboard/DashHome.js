bc2fd5526388a1f72b73d3e01ad325496317f1e2
SoteroX <soterox13@gmail.com>
2019-08-03 05:14:34 -0500

    change default home page design

--- client/src/components/dashboard/DashHome.js
+++ client/src/components/dashboard/DashHome.js
@@ -1,15 +1,40 @@
-import React from "react";
+import React, { Component } from "react";
+import { Button, Header, Icon, Segment } from "semantic-ui-react";
+import AddProjectModal from "../modals/AddProjectModal";
 
-export default function DashHome() {
-  return (
-    <div
-      className="flex flex-justify-center flex-align-items-center"
-      style={{ height: "95%" }}
-    >
-      <h1>
-        No Project has been selected, please select a project or create a new
-        one
-      </h1>
-    </div>
-  );
+export class DashHome extends Component {
+  state = {
+    openModal: false
+  };
+  handleOpenModal = () => {
+    this.setState({ openModal: !this.state.openModal });
+  };
+  render() {
+    const { openModal } = this.state;
+    console.log("open:", openModal);
+    return (
+      <div
+        className="flex flex-justify-center flex-align-items-center"
+        style={{ height: "95%" }}
+      >
+        <Segment style={{ height: "100%", width: "100%" }} placeholder>
+          <Header icon>
+            <Icon name="pdf file outline" />
+            No project is selected, please select project or create new one.
+          </Header>
+          <Button onClick={this.handleOpenModal} primary>
+            Create Project
+          </Button>
+        </Segment>
+        {openModal && (
+          <AddProjectModal
+            open={openModal}
+            handleOpenModal={this.handleOpenModal}
+          />
+        )}
+      </div>
+    );
+  }
 }
+
+export default DashHome;
